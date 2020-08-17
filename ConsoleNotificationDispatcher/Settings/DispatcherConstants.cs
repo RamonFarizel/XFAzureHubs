7237242b8e0fa2d25ac44db36010987a0a39cb77
Ramon Farizel <ramonfarizel@gmail.com>
2020-08-17 11:10:30 -0300

    Configurando console para enviar notifications

---
+++ ConsoleNotificationDispatcher/Settings/DispatcherConstants.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace ConsoleNotificationDispatcher.Settings
+{
+    public static class DispatcherConstants
+    {
+        public static string[] SubscriptionTags { get; set; } = { "default" };
+        public static string NotificationHubName { get; set; } = "POCHubNotifications";
+        public static string FullAccessConnectionString { get; set; } = "Endpoint=sb://pochubnamespace.servicebus.windows.net/;SharedAccessKeyName=DefaultFullSharedAccessSignature;SharedAccessKey=JTY/cE9MdxNOtkCzBEUFST8YYluIVhEQDvUGmpVBip0=";
+    }
+}
--- XFAzureHubs.iOS/AppDelegate.cs
+++ XFAzureHubs.iOS/AppDelegate.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 
 using Foundation;
 using UIKit;
+using UserNotifications;
+using WindowsAzure.Messaging;
+using XFAzureHubs.Settings;
 
 namespace XFAzureHubs.iOS
 {
@@ -23,9 +27,137 @@ namespace XFAzureHubs.iOS
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             global::Xamarin.Forms.Forms.Init();
+
+            UNUserNotificationCenter.Current.Delegate = new NotificationCenterDelegate();
+
             LoadApplication(new App());
 
+            
+            RegisterForRemoteNotifications();
+
             return base.FinishedLaunching(app, options);
+            
+
+        }
+
+        void RegisterForRemoteNotifications()
+        {
+            // register for remote notifications based on system version
+            if (UIDevice.CurrentDevice.CheckSystemVersion(10, 0))
+            {
+                UNUserNotificationCenter.Current.RequestAuthorization(UNAuthorizationOptions.Alert |
+                    UNAuthorizationOptions.Sound |
+                    UNAuthorizationOptions.Sound,
+                    (granted, error) =>
+                    {
+                        if (granted)
+                            InvokeOnMainThread(UIApplication.SharedApplication.RegisterForRemoteNotifications);
+                    });
+            }
+            else if (UIDevice.CurrentDevice.CheckSystemVersion(8, 0))
+            {
+                var pushSettings = UIUserNotificationSettings.GetSettingsForTypes(
+                UIUserNotificationType.Alert | UIUserNotificationType.Badge | UIUserNotificationType.Sound,
+                new NSSet());
+
+                UIApplication.SharedApplication.RegisterUserNotificationSettings(pushSettings);
+                UIApplication.SharedApplication.RegisterForRemoteNotifications();
+            }
+            else
+            {
+                UIRemoteNotificationType notificationTypes = UIRemoteNotificationType.Alert | UIRemoteNotificationType.Badge | UIRemoteNotificationType.Sound;
+                UIApplication.SharedApplication.RegisterForRemoteNotificationTypes(notificationTypes);
+            }
+        }
+
+        public override void RegisteredForRemoteNotifications(UIApplication application, NSData deviceToken)
+        {
+            var Hub = new SBNotificationHub(AppConstants.ListenConnectionString, AppConstants.NotificationHubName);
+
+            // update registration with Azure Notification Hub
+            Hub.UnregisterAll(deviceToken, (error) =>
+            {
+                if (error != null)
+                {
+                    Debug.WriteLine($"Unable to call unregister {error}");
+                    return;
+                }
+
+                var tags = new NSSet(AppConstants.SubscriptionTags.ToArray());
+                Hub.RegisterNative(deviceToken, tags, (errorCallback) =>
+                {
+                    if (errorCallback != null)
+                    {
+                        Debug.WriteLine($"RegisterNativeAsync error: {errorCallback}");
+                    }
+                });
+
+                var templateExpiration = DateTime.Now.AddDays(120).ToString(System.Globalization.CultureInfo.CreateSpecificCulture("en-US"));
+                Hub.RegisterTemplate(deviceToken, "defaultTemplate", AppConstants.APNTemplateBody, templateExpiration, tags, (errorCallback) =>
+                {
+                    if (errorCallback != null)
+                    {
+                        if (errorCallback != null)
+                        {
+                            Debug.WriteLine($"RegisterTemplateAsync error: {errorCallback}");
+                        }
+                    }
+                });
+            });
+        }
+
+        public override void FailedToRegisterForRemoteNotifications(UIApplication application, NSError error)
+        {
+            base.FailedToRegisterForRemoteNotifications(application, error);
+        }
+
+        public override void ReceivedRemoteNotification(UIApplication application, NSDictionary userInfo)
+        {
+            ProcessNotification(userInfo, false);
+        }
+
+        void ProcessNotification(NSDictionary options, bool fromFinishedLaunching)
+        {
+            // make sure we have a payload
+            if (options != null && options.ContainsKey(new NSString("aps")))
+            {
+                // get the APS dictionary and extract message payload. Message JSON will be converted
+                // into a NSDictionary so more complex payloads may require more processing
+                NSDictionary aps = options.ObjectForKey(new NSString("aps")) as NSDictionary;
+                string payload = string.Empty;
+                NSString payloadKey = new NSString("alert");
+                if (aps.ContainsKey(payloadKey))
+                {
+                    payload = aps[payloadKey].ToString();
+                }
+
+                if (!string.IsNullOrWhiteSpace(payload))
+                {
+                    Debug.WriteLine(payload);
+                }
+
+            }
+            else
+            {
+                Debug.WriteLine($"Received request to process notification but there was no payload.");
+            }
+        }
+    }
+
+    [Preserve(AllMembers = true)]
+    public class NotificationCenterDelegate : UNUserNotificationCenterDelegate
+    {
+        public override void WillPresentNotification(UNUserNotificationCenter center,
+            UNNotification notification, Action<UNNotificationPresentationOptions> completionHandler)
+        {
+            // Do something with the notification
+            Console.WriteLine("Active Notification: {0}", notification);
+
+            // Tell system to display the notification anyway or use
+            // `None` to say we have handled the display locally.
+            completionHandler(UNNotificationPresentationOptions.Alert |
+                              UNNotificationPresentationOptions.Badge |
+                              UNNotificationPresentationOptions.Sound);
         }
     }
 }
